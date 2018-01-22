06d11b772a846274272c4f83fa5615b44be3a53a
Matt Rixman <matt.rixman@clover.com>
2018-01-22 16:44:00 +0000

    switching to test_promote

--- undo_promote.sh
+++ undo_promote.sh
@@ -7,4 +7,4 @@
 # promote will update some tables in test_meta
 # this command will replace those tables with their values from meta
 # (essentially undoing the promote)
-mysqldump -uroot -ptest meta app_metered_event charge merchant_app_charge merchant_plan_charge salesforce_config | mysql -uroot -ptest -D test_meta
+mysqldump -uroot -ptest meta app_metered_event charge merchant_app_charge merchant_plan_charge salesforce_config | mysql -uroot -ptest -D test_promote
